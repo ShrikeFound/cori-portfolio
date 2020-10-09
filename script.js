f1606cd2aee9e6a76c8db949ac0a1c5cebfce81b
Alfonso <61892420+ShrikeFound@users.noreply.github.com>
2020-10-08 22:46:35 -0700

    start script for color change
    added script for adding span to name. Regex is a pain :S

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 console.log("works!");
 
 const mainMenu = document.getElementById("main-menu");
+const name = document.getElementById("name");
 
 function toggleMenu() {
   mainMenu.classList.toggle("open");
 }
+
+function countChars(element) {
+  element.innerHTML = element.innerHTML
+    .replace(/\w[^CW]/g, '<span class="hightlightable">$&</span>')
+    .replace(/[CW]/g, '<span class="hightlightable highlighted">$&</span>')
+    .replace(/\s/g, " ");
+}
+countChars(name);
+// countChars(degree);
